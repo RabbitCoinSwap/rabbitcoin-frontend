5d93ec5761d34665c4f107c57508686dcddc4782
Rabbit Chef <163091962+rabbitcoinchef@users.noreply.github.com>
2024-03-30 19:05:23 +0700

    first commit

--- src/state/info/queries/types.ts
+++ src/state/info/queries/types.ts
@@ -51,10 +51,10 @@ export interface TokenDayDatasResponse {
 }
 
 // Footprint is the same, declared just for better readability
-export type PancakeDayData = TokenDayData
+export type rabbitCoinDayData = TokenDayData
 
-export interface PancakeDayDatasResponse {
-  pancakeDayDatas: PancakeDayData[]
+export interface rabbitCoinDayDatasResponse {
+  rabbitCoinDayDatas: rabbitCoinDayData[]
 }
 
 export interface PairDayData {
