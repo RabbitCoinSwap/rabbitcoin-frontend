c577c7364128a1db82f56f07971776ffdf33fc70
Rabbit Chef <163091962+rabbitcoinchef@users.noreply.github.com>
2024-04-01 01:58:31 +0700

    Update endpoints.ts

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -13,7 +13,7 @@ export const SNAPSHOT_HUB_API = `${SNAPSHOT_BASE_URL}/api/message`
 export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
 
 export const INFO_CLIENT = 'https://api.thegraph.com/subgraphs/name/rabbitcoinchef/exchange-mini'
-export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
+export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/rabbitcoinchef/blocks'
 export const GRAPH_API_NFTMARKET = process.env.NEXT_PUBLIC_GRAPH_API_NFT_MARKET
 export const GRAPH_HEALTH = 'https://api.thegraph.com/index-node/graphql'
 export const BIT_QUERY = 'https://graphql.bitquery.io'
